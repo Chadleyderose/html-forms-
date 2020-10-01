59614bcc0e9457b3cb300a4fee916015671460d8
chadleyderose <chadleyderose1911201@gmail.com>
2020-10-01 22:19:25 +0200

    new update

--- Main.py
+++ Main.py
@@ -1,24 +1,24 @@
-#Calculating the increase in salary
+# Calculating the increase in salary
 print("--The Bright Light Company employees Salary increase--")
 
-Department_code = input("Enter Department code: ")
-anSalary = input("Enter Anual salary: ")
+Deptcode = input("Enter Department code: ")
+anSalary = int(input("Enter Anual salary: "))
 
 a = 0.072
 b = 0.068
-other = 0.063
+o = 0.063
 
-if Department_code == a:
+if Deptcode == a:
     increase_anSalary = anSalary + (anSalary * a)
-    monthly_salary = increase_anSalary / 12
-    print(monthly_salary)
-elif Department_code == b:
+    monsalary = increase_anSalary / 12
+    print(monsalary)
+elif Deptcode == b:
     increase_anSalary = anSalary + (anSalary * b)
-    monthly_salary = increase_anSalary / 12
-    print(monthly_salary)
-elif Department_code == other:
-    increase_anSalary = anSalary + (anSalary * other)
-    monthly_salary = increase_anSalary / 12
-    print(monthly_salary)
+    monsalary = increase_anSalary / 12
+    print(monsalary)
+elif Deptcode == o:
+    increase_anSalary = anSalary + (anSalary * o)
+    monsalary = increase_anSalary / 12
+    print(monsalary)
 else:
     print("please check your DepartmentCode and restart")
